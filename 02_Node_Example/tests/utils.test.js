31bb2cd1642c06c34f614079ab86c866b6672331
PliTheGeek <dhafin.kamil@gmail.com>
2025-11-05 14:03:33 +0700

    Add initial test cases for app and utility functions

---
+++ 02_Node_Example/tests/utils.test.js
@@ -0,0 +1,9 @@
+const { tambah, kurang } = require('../src/utils');
+
+test('menjumlahkan dua angka', () => {
+    expect(tambah(2, 3)).toBe(5);
+});
+
+test('mengurangkan dua angka', () => {
+    expect(kurang(5, 3)).toBe(2);
+}); 
